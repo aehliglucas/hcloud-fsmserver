8e32c767a2ca8588dcfe06dd531cd3148f2f9885
aehliglucas <lucas@aehlig.com>
2024-07-11 19:37:37 +0200

    add FSMS start functionality

--- apply.sh
+++ apply.sh
@@ -23,4 +23,4 @@ terraform apply \
 echo "Adding SSH private key from $ANSIBLE_SSH_PRIVKEY to ssh-agent"
 
 cd ../ansible 
-ansible-playbook -i hosts --private-key $ANSIBLE_SSH_PRIVKEY main.yml
+ansible-playbook -i hosts --private-key $ANSIBLE_SSH_PRIVKEY --extra-vars "fsms_port=$FSMS_PORT" main.yml
